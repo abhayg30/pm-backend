626ce37a42eab33af244fa4dfe8b26488dc48c9c
abhayg30 <abhaygupta.3003@gmail.com>
2024-07-29 14:05:12 +1000

    Update settings.py

--- powerpuff/settings.py
+++ powerpuff/settings.py
@@ -23,7 +23,7 @@ USE_TZ = False
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
